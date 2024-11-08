45bae96e68380c96121b5006288bbfa1bdfd8333
AliReza Beigy <alireza.beigy.rb@gmail.com>
2024-11-08 19:43:48 +0330

    refactor: add issue sample

--- App.tsx
+++ App.tsx
@@ -1,118 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useRef, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    Platform,
+    Pressable,
+    SafeAreaView,
+    StyleSheet, Text, TextInput,
+    View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    const inputRef = useRef<TextInput>(null);
+    const [showPass, setShowPass] = useState(false);
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.inputContainer}>
+                <View>
+                    <View style={{
+                        width: '100%',
+                        borderRadius: 5,
+                        marginVertical: 10,
+                        flexDirection: 'row',
+                        paddingHorizontal: 5,
+                        borderWidth: 1,
+                        borderColor: 'blue',
+                    }}>
+                        <TextInput
+                            ref={inputRef}
+                            style={{
+                                flex: 1,
+                                color: 'red',
+                                paddingTop: Platform.OS === 'ios' ? 15 : undefined,
+                                paddingBottom: Platform.OS === 'ios' ? 15 : undefined,
+                                paddingHorizontal: Platform.OS === 'ios' ? 5 : undefined,
+                            }}
+                            secureTextEntry={!showPass}
+                            onChangeText={v => {
+                                let value = v;
+                                inputRef.current?.setNativeProps({text: value});
+                            }}
+                        />
+                        <Pressable onPress={() => setShowPass(!showPass)} style={{
+                            marginStart: 'auto',
+                            marginHorizontal: 10,
+                            justifyContent: 'center',
+                        }}>
+                            <Text>{showPass ? "Hide" : "Show"}</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    inputContainer: {
+        width: '80%',
+    },
+    textInput: {
+        height: 40,
+        borderColor: '#ccc',
+        color: 'red',
+        borderWidth: 1,
+        marginBottom: 12,
+        paddingHorizontal: 10,
+    },
 });
 
 export default App;
